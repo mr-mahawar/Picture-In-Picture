c7df7cfa40db43bc61a29f9e6c6ca4aebaefdac4
Tilak Mahawar <mahawart99@gmail.com>
2021-03-09 13:32:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    }
+    catch(error) {
+        // error message here
+        console.log('Whoops Error:',error);
+    }
+}
+
+// for button
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+})
+
+// on load
+selectMediaStream();
